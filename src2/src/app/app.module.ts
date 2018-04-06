395d7990e19e6cddcca9de5e0d9546da8736122f
Francisco <franjosato@gmail.com>
2018-04-06 00:19:01 -0400

    Primer Commit 1

--- src2/src/app/app.module.ts
+++ src2/src/app/app.module.ts
@@ -3,17 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { PrincipalPage } from '../pages/principal/principal';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { RegistroPage } from '../pages/registro/registro';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    PrincipalPage,
-    RegistroPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -22,9 +19,7 @@ import { RegistroPage } from '../pages/registro/registro';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    PrincipalPage,
-    RegistroPage,
+    HomePage
   ],
   providers: [
     StatusBar,
